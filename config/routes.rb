91d7aa7ed438134f88ee4aa5f762fc3f4795e517
Kyle Geske <stungeye@gmail.com>
2017-02-09 19:58:09 +0000

    Added the home controller and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'about' => 'main#about', as: 'about'
+
+  get 'contact' => 'main#contact', as: 'contact'
+
+  root 'main#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
